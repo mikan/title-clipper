e1de2f03aeb594ee1bc754309fba0e3f07b02fdb
Yutaka Kato <mikan@aomikan.org>
2025-03-12 02:11:24 +0900

    doc: fix comment typo

--- test/content.test.mjs
+++ test/content.test.mjs
@@ -4,7 +4,7 @@ import * as content from "../src/content.js";
 
 test("any patterns", () => {
   [
-    // [["title","url"],["expected]"
+    // [["title","url"],["expected"]
     [["title", "https://example.com/1"], "title\nhttps://example.com/1"],
     [[undefined, "https://example.com/2"], "https://example.com/2"],
   ].forEach((sample) => {
